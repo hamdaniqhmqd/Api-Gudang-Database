3979419259378647a5760d913f063e90ec642609
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-11-25 20:30:19 +0700

    repair message

--- src/app.js
+++ src/app.js
@@ -11,7 +11,25 @@ app.use(supplierRoutes);
 app.use(transaksiRoutes);
 app.use(barangRoutes);
 
+// Root route untuk menampilkan rekomendasi daftar endpoint
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the API. Here are the available endpoints:',
+    endpoints: {
+      '/users': 'Get list of users',
+      '/users/:id': 'Get user by ID',
+      '/suppliers': 'Get list of suppliers',
+      '/suppliers/:id': 'Get supplier by ID',
+      '/barang': 'Get list of barang',
+      '/barang/:id': 'Get barang by ID',
+      '/transaksi': 'Get list of transaksi',
+      '/transaksi/:id': 'Get transaksi by ID',
+    }
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
